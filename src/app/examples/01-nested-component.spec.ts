d149f9c1548f6a656c46ac172ddcfee856865075
Khairuddin Ni'am <niam.nfinity@gmail.com>
2023-03-22 20:52:47 +0700

    feat: add more test examples

---
+++ src/app/examples/01-nested-component.spec.ts
@@ -0,0 +1,22 @@
+import { fireEvent, render, screen } from '@testing-library/angular'
+
+import { NestedButtonComponent, NestedContainerComponent, NestedValueComponent } from './01-nested-component'
+
+test('renders the current value and can increment and decrement', async () => {
+  await render(NestedContainerComponent, {
+    declarations: [NestedButtonComponent, NestedValueComponent],
+  })
+
+  const incrementControl = screen.getByRole('button', { name: /increment/i })
+  const decrementControl = screen.getByRole('button', { name: /decrement/i })
+  const valueControl = screen.getByTestId('value')
+
+  expect(valueControl).toHaveTextContent('0')
+
+  fireEvent.click(incrementControl)
+  fireEvent.click(incrementControl)
+  expect(valueControl).toHaveTextContent('2')
+
+  fireEvent.click(decrementControl)
+  expect(valueControl).toHaveTextContent('1')
+})
